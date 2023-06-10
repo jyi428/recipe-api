3070244bc74b61aa311f6c23f0ebbe7f706981ed
Jonathan Yi <jyi428@users.noreply.github.com>
2023-06-10 17:01:16 -0500

    Sample calc test and python code

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Caclculator functions
+"""
+
+def add(x, y):
+    """Add x and y returns result"""
+    return x + y
+
+def subtract(x, y):
+    """Subtract x and y returns result"""
+    return y - x
